2230f0bbcbcd1fd9b1558366fc9e0e04834b9f6d
lealinnea <leag0905@gmail.com>
2022-05-09 21:26:27 -0500

    intern testing

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -1,16 +1,16 @@
-const employee = require('../lib/intern');
+const intern = require('../lib/intern');
 
 describe('intern', () =>{
     describe('getSchool', () =>{
         it(`should send interns school`, () => {
-            const newEmployee = new employee('name', '1', 'name.email');
-            expect(newEmployee.getName()).toBe("Employee's Name: Name")
+            const newEmployee = new intern('name', '1', 'name.email', 'Ohio State University');
+            expect(newEmployee.getSchool()).toBe("Person is going to Ohio State University")
         });
     });
         describe('getEmpRoll', () =>{
             it(`should send employees roll`, () => {
-                const newEmployee = new employee('name', '1', 'name.email');
-                expect(newEmployee.getRoll()).toBe(`Employee's Roll: Intern`)
+                const newEmployee = new intern('name', '1', 'name.email');
+                expect(newEmployee.getRoll()).toBe(`Roll: Intern`)
             });
         });
 
